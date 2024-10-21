e3bcc1cfc74c6bcb9f8a453cacbd3b4112505c3c
Mark Veerasingam <G00403931@atu.ie>
2024-10-21 16:40:04 +0100

    added GlobalExceptionHandler

---
+++ src/main/java/ie/atu/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package ie.atu;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Object>handleRuntimeException(RuntimeException ex){
+        Map<String, String> errorResponse = new HashMap();
+        errorResponse.put("Error", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
