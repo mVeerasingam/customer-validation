8c4760fe7d9d4418fc1764fdad0775b4108da4f7
Mark Veerasingam <G00403931@atu.ie>
2024-10-21 16:12:05 +0100

    first commit

---
+++ src/main/java/ie/atu/CustomerVallidationApplication.java
@@ -0,0 +1,13 @@
+package ie.atu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerVallidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerVallidationApplication.class, args);
+    }
+
+}
